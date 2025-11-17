bc9a63c38450d32319de14d84d4b2e3bddc06a41
ZGV2 <dev@bogdanbpeterson.com>
2025-11-17 22:54:29 +0200

    fix: generalize dir creation

--- src/utils/check-and-create-dir.ts
+++ src/utils/check-and-create-dir.ts
@@ -1,17 +1,19 @@
 import { mkdir, stat } from "node:fs/promises";
 import exitWithError from "./exit-with-error";
 
-export default async function createExportsDirectory(): Promise<boolean> {
+export default async function checkAndCreateDirectory(
+  dirname: string,
+): Promise<boolean> {
   try {
-    const stats = await stat("exports");
+    const stats = await stat(dirname);
 
     if (!stats.isDirectory()) {
-      exitWithError("exports already exists and it's not a directory");
+      exitWithError(`${dirname} already exists and it's not a directory`);
     }
     return true;
   } catch (error) {
     if ((error as ErrnoException).code === "ENOENT") {
-      await mkdir("exports");
+      await mkdir(dirname);
       return true;
     }
   }
